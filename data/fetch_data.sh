149bf4b2493e7f3a5dd2ae58f115ed818ec01515
Harry Li <lidatou0708@gmail.com>
2025-05-06 23:18:20 +0000

    updates

---
+++ data/fetch_data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+## get data files
+mkdir -p zipped_files
+gdown 1463tF6_IcVcCQCZudCFU0fz1_GbHA8ak --output zipped_files/DC1.zarr.zip
+gdown 18KynQo9F-YKREG5kDTI9InqP7NG2L0cx --output zipped_files/DC5.zarr.zip
+gdown 1f7akUHKC4UFm_X9KWRZVsUkYOlbHUA5U --output zipped_files/UC1_I.zarr.zip
+gdown 1UGhC68uyaVQBFXsrvkHveU4rFiwjgTlm --output zipped_files/UC1_NI.zarr.zip
+gdown 1OLIh0rbCBwDifYDgT4fR-mhltHCxDr34 --output zipped_files/UC6_I.zarr.zip
+gdown 1RlJDxHXTkZffhfSS-ZXzt8vRWaoRumUp --output zipped_files/UC6_NI.zarr.zip
+gdown 1YK0DqV1EffewNmXDb7lITNwK-BE2bWv4 --output zipped_files/UC7_I.zarr.zip
+gdown 1e7qAk9A_z6uO-jhnXrw3KDIrML3tqG02 --output zipped_files/UC9_I.zarr.zip
+
+## unzip files
+mkdir -p raw
+for file in zipped_files/*.zip; do
+    base=$(basename "$file" .zip)
+    target_dir="raw/$base"
+    mkdir -p "$target_dir"
+    echo "Unzipping $file to $target_dir..."
+    unzip "$file" -d "$target_dir" && rm "$file"
+done
