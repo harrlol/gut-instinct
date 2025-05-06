cae98181caff7302834404e8d84df145c9d5f5dd
Harry Li <lidatou0708@gmail.com>
2025-05-06 18:42:28 +0000

    ignores

--- src/data.py
+++
@@ -1,6 +0,0 @@
-from config import DATA_PATH
-import os
-
-
-print(f"Loading from: {DATA_PATH}")
-filepath = os.path.join(DATA_PATH)
---
+++ src/data/patch.py
@@ -0,0 +1,90 @@
+import torch
+import numpy as np
+from skimage.measure import regionprops
+from torch.utils.data import Dataset
+
+class PatchCells(Dataset):
+    """
+    This class takes in an histology image,
+    list of cell ids that is supposedly in that image
+    a dictionary that maps cell ids to their centroid coordinates,
+    and a patch size.
+    """
+    def __init__(self, image, cell_ids, cell_coords, patch_size=32):
+        self.image = image
+        self.cell_ids = cell_ids
+        self.cell_coords = cell_coords
+        self.patch_size = patch_size
+
+    # do a filtering of the cell ids
+        self.valid_cell_ids = [id for id in cell_ids if id in cell_coords]
+
+    def __len__(self):
+        return len(self.valid_cell_ids)
+
+    # takes in a cell id, returns a {key: cell id, value: patch in tensor format}
+    def __getitem__(self, idx):
+        cell_id = self.valid_cell_ids[idx]
+        x, y = self.cell_coords[cell_id]
+
+        half_patch = self.patch_size // 2
+
+        # taking care of edge cases as well
+        x_start = max(0, x - half_patch)
+        x_end = min(self.image.shape[0], x + half_patch)
+        y_start = max(0, y - half_patch)
+        y_end = min(self.image.shape[1], y + half_patch)
+
+        # create a 0 patch first
+        patch = np.zeros((self.patch_size, self.patch_size, 3), dtype=np.float32)
+
+        # populate with actual data, note the case where cell is at edge of image
+        patch_from_in = self.image[y_start:y_end, x_start:x_end]
+        patch[:patch_from_in.shape[0], :patch_from_in.shape[1], :] = patch_from_in
+
+        # normalize
+        patch = patch / 255.0
+
+        # pytorch tensor
+        patch = np.transpose(patch, (2, 0, 1))
+
+        return {
+            'cell_id': cell_id,
+            'patch': torch.tensor(patch, dtype=torch.float32)
+        }
+
+def get_patches(sdata, random_seed=209):
+    # pull training cells ids into a list
+    split_cell_id = sdata["cell_id-group"].obs.query("group == 'train'")["cell_id"].values
+
+    # get mask, pull regions
+    he_nuc_mask = sdata['HE_nuc_original'][0, :, :].to_numpy()
+    regions = regionprops(he_nuc_mask)
+
+    # pull centroid coordinate of each cell's regions
+    # dict has key=cell id and value=centroid coordinate
+    cell_coords = {}
+    for props in regions:
+        cid = props.label
+        if cid in split_cell_id:
+            y_center, x_center = int(props.centroid[0]), int(props.centroid[1])
+            cell_coords[cid] = (x_center, y_center)
+
+    # assemble the patch dataset
+    he_image = np.transpose(sdata['HE_original'].to_numpy(), (1, 2, 0))
+
+    np.random.seed(random_seed)
+    shuffled = np.random.permutation(split_cell_id)
+    total_len = len(split_cell_id)
+    train_len = int(0.7 * total_len)
+    val_len = int(0.2 * total_len)
+    train_ids = shuffled[:train_len]
+    val_ids = shuffled[train_len:train_len + val_len]
+    test_ids = shuffled[train_len + val_len:]
+
+    # create dataset objects
+    dataset_patch_train = PatchCells(he_image, train_ids, cell_coords, patch_size=32)
+    dataset_patch_val = PatchCells(he_image, val_ids, cell_coords, patch_size=32)
+    dataset_patch_test = PatchCells(he_image, test_ids, cell_coords, patch_size=32)
+
+    return dataset_patch_train, dataset_patch_val, dataset_patch_test
